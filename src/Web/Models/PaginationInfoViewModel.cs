34d333ecd89919740e2a30bc899120a7daefba9e
Sinancdr <Sinantasyapar@gmail.com>
2023-09-18 10:15:44 +0300

    Pagination numbers

--- src/Web/Models/PaginationInfoViewModel.cs
+++ src/Web/Models/PaginationInfoViewModel.cs
@@ -17,5 +17,44 @@
         public int RangeStart => (PageId - 1) * Constants.ITEMS_PER_PAGE + 1;
 
         public int RangeEnd => RangeStart + ItemsOnPage - 1;
+
+        public int[] PageNumbers => Pagination(PageId, TotalPages);
+
+        private static int[] Pagination(int current, int last)
+        {
+            int delta = 2;
+            int left = current - delta;
+            int right = current + delta + 1;
+            var range = new List<int>();
+            var rangeWithDots = new List<int>();
+            int? l = null;
+
+            for (var i = 1; i <= last; i++)
+            {
+                if (i == 1 || i == last || i >= left && i < right)
+                {
+                    range.Add(i);
+                }
+            }
+
+            foreach (var i in range)
+            {
+                if (l != null)
+                {
+                    if (i - l == 2)
+                    {
+                        rangeWithDots.Add(l.Value + 1);
+                    }
+                    else if (i - l != 1)
+                    {
+                        rangeWithDots.Add(-1);
+                    }
+                }
+                rangeWithDots.Add(i);
+                l = i;
+            }
+
+            return rangeWithDots.ToArray();
+        }
     }
 }
